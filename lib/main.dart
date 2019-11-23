6fa9fd8c116ed7850398a729656e4e2921bcbe59
Albin <albinbaby26895@gmail.com>
2019-11-24 01:18:50 +0530

    change of text

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('Startup Name Generatorchksnhsjcj'),
       ),
       body: _buildSuggestions(),
     );
